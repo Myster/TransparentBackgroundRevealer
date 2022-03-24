4e833239c425801bae35a7f446b0830e082cdb03
Murray Roke <murray@roke.co.nz>
2022-03-24 13:00:42 +1300

    Create transparent-background-revealer.js

---
+++ transparent-background-revealer.js
@@ -0,0 +1,3 @@
+document.children[0].style.backgroundImage = `url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" fill-opacity="0.3" width="50" height="50" ><g fill="grey"><rect x="50" width="50" height="50" /><rect y="50" width="50" height="50" /></g><rect width="50" height="50" /><rect y="50" x="50" width="50" height="50" /></svg>')`;
+document.body.style.background = null;
+document.body.children[0].style.background=null;
